a236d7cc1abc3cf7c3e61a5e1564e28b9f3210db
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-11-29 14:23:19 +0530

    authorization middleware created successfully

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,22 +1,25 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const authMiddleware=(req,res,next) =>{
-    const token=req.header("Authorization");
-    if(!token){
-        return res.status(401).json({
-            error:"No Token Provided,authorization Denied"
-        })
-    }
-    try{
-        const decoded=jwt.verify(token,process.env.JWT_SECRET);
-        req.user=decoded;
-        next();
-    }
-    catch{
-        return res.status(400).json({
-            error:"Invalid Token"
-        })
-    }
-}
-module.exports=authMiddleware;
+const authMiddleware = (req, res, next) => {
+  let token = req.header("Authorization");
+  if (!token) {
+    return res.status(401).json({
+      error: "No Token Provided,authorization Denied",
+    });
+  }
+  // Remove "Bearer " if exists
+  if (token.startsWith("Bearer ")) {
+    token = token.split(" ")[1];
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    return res.status(400).json({
+      error: "Invalid Token",
+    });
+  }
+};
+module.exports = authMiddleware;
